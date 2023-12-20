c62421890500a79630f84f531c96166524a9c504
mayankHype <mayank.pandey@hypecard.in>
2023-12-20 10:51:04 +0530

    df

---
+++ android/app/src/main/kotlin/com/application/shorebird/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.application.shorebird
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/feature/paymentgateway/data/remote.dart
@@ -0,0 +1,7 @@
+import 'dart:convert';
+import 'dart:developer';
+
+const kAndroidUserAgent =
+    "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Mobile Safari/537.36";
+
+
